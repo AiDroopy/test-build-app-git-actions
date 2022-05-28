fc7177ee2066bfda133af1b48552b5a067b1d123
AiDroopy <bjorn.milebjork@gmail.com>
2022-05-28 14:37:11 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        
+         <p> This is a app ready for deployment</p>
+         <p>Target: dev2</p>
+         <p>type: nodejs</p>
+         <p>Author: Bjorn</p>
+         <p>Path: c:\users\bjpes\dev2_tests\a-simple-nodejsApp\dev2-deploy-app</p>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
